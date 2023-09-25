369ade7185bcce310483c3f24e09ed94cc39f051
Испольнов Евгений Владиславович <ispolnovev@fayrix.com>
2023-09-25 17:37:25 +0300

    first commit

---
+++ CinemaRoulette/CinemaRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaRouletteApp.swift
+//  CinemaRoulette
+//
+//  Created by Евгений Испольнов on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CinemaRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
