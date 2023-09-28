f0d60b6295b519d5f1ebca1842467d05989123e0
Испольнов Евгений Владиславович <ispolnovev@fayrix.com>
2023-09-28 17:51:15 +0300

    Добавлено окно краткой информации о фильме

--- CinemaRoulette/Views/FilmView.swift
+++ CinemaRoulette/Views/FilmView.swift
@@ -12,22 +12,86 @@ struct FilmView: View {
     let film: FilmsTopModel.Film
     
     var body: some View {
-        VStack {
-            if let urlString = film.posterUrlPreview {
-                AsyncImage(url: URL(string: urlString)) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 48, height: 72)
-                } placeholder: {
-                    Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 48, height: 72)
+        HStack(spacing: 16) {
+            if let urlString = film.posterUrl {
+                VStack {
+                    AsyncImage(url: URL(string: urlString)) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        Rectangle()
+                            .fill(Color.gray)
+                    }
+                    .frame(width: 120, height: 180)
+                    .cornerRadius(8)
+                    Spacer()
                 }
-
             }
-            Text(film.nameRu ?? "")
+            VStack(spacing: 4) {
+                if let nameRu = film.nameRu {
+                    Text(nameRu)
+                        .font(Font.system(.headline))
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                }
+                if let nameEn = film.nameEn {
+                    Text(nameEn)
+                        .font(Font.system(.subheadline))
+                        .foregroundColor(Color.gray)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                }
+                if let genres = film.genres, !genres.isEmpty {
+                    let genresText: String = {
+                        if genres.count == 1 {
+                            return "Жанр: " + genres[0].genre
+                        } else {
+                            return "Жанры: " + genresToString(genres: genres)
+                        }
+                    }()
+                    Text(genresText)
+                        .font(Font.system(.subheadline))
+                        .foregroundColor(Color.gray)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                        .padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+                }
+                if let countries = film.countries, !countries.isEmpty {
+                    let countriesText: String = {
+                        if countries.count == 1 {
+                            return "Страна: " + countries[0].country
+                        } else {
+                            return "Страны: " + countriesToString(countries: countries)
+                        }
+                    }()
+                    Text(countriesText)
+                        .font(Font.system(.subheadline))
+                        .foregroundColor(Color.gray)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                        .padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+                }
+                if let filmLength = film.filmLength {
+                    Text("Продолжительность: " + filmLength)
+                        .font(Font.system(size: 12, weight: .light))
+                        .foregroundColor(Color.gray)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                        .padding(EdgeInsets(top: 8, leading: 0, bottom: 0, trailing: 0))
+                }
+            }
+            .frame(maxHeight: .infinity, alignment: .topLeading)
         }
+        .frame(maxWidth: .infinity, alignment: .topLeading)
+        .padding(EdgeInsets(top: 48, leading: 32, bottom: 32, trailing: 32))
+    }
+    
+}
+
+extension FilmView {
+    
+    private func genresToString(genres: [FilmsTopModel.Film.Genre]) -> String {
+        return genres.map({ $0.genre }).joined(separator: ", ")
+    }
+    
+    private func countriesToString(countries: [FilmsTopModel.Film.Country]) -> String {
+        return countries.map({ $0.country }).joined(separator: ", ")
     }
     
 }
--- CinemaRoulette/Views/FilmsListView.swift
+++ CinemaRoulette/Views/FilmsListView.swift
@@ -11,47 +11,72 @@ struct FilmsListView: View {
     
     @ObservedObject var viewModel: FilmsListViewModel
     
+    @State var selectedFilm: FilmsTopModel.Film?
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach(viewModel.films.enumerated().map({ $0 }), id: \.element.filmId) { index, film in
-                    HStack {
-                        if let urlString = film.posterUrlPreview {
-                            AsyncImage(url: URL(string: urlString)) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 48, height: 72)
-                            } placeholder: {
-                                Rectangle()
-                                    .fill(Color.gray)
-                                    .frame(width: 48, height: 72)
-                            }
-            
-                        }
-                        VStack(spacing: 8) {
-                            Text(film.nameRu ?? "")
-                                .font(Font.system(size: 14, weight: .medium))
-                                .foregroundColor(Color.black)
-                                .frame(maxWidth: .infinity, alignment: .topLeading)
-                            Text(film.nameEn ?? "")
-                                .font(Font.system(size: 12, weight: .light))
-                                .foregroundColor(Color.gray)
-                                .frame(maxWidth: .infinity, alignment: .topLeading)
-                        }
-                        Spacer()
-                        Image(systemName: "star")
-                        Text(film.rating ?? "")
-                            .font(Font.system(size: 14, weight: .light))
-                            .foregroundColor(Color.gray)
-                    }
-                    .onAppear { viewModel.requestMoreItemsIfNeeded(itemsCount: viewModel.films.count, index: index) }
+                ForEach(viewModel.films.enumerated().map({ $0 }), id: \.element.id) { index, film in
+                    FilmListItemView(film: film, selectedFilm: $selectedFilm)
+                        .onAppear { viewModel.requestMoreItemsIfNeeded(itemsCount: viewModel.films.count, index: index) }
                 }
             }
             .listStyle(.inset)
+            .sheet(item: $selectedFilm) { film in
+                FilmView(film: film)
+                    .presentationDetents([.height(300), .medium, .large])
+                    .presentationDragIndicator(.automatic)
+            }
         }
         .navigationTitle("Топ 250")
     }
+    
+}
+
+extension FilmsListView {
+    
+    private struct FilmListItemView: View {
         
+        let film: FilmsTopModel.Film
+        
+        @Binding var selectedFilm: FilmsTopModel.Film?
+        
+        var body: some View {
+            HStack {
+                if let urlString = film.posterUrlPreview {
+                    AsyncImage(url: URL(string: urlString)) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        Rectangle()
+                            .fill(Color.gray)
+                    }
+                    .frame(width: 48, height: 72)
+                }
+                VStack(spacing: 8) {
+                    Text(film.nameRu ?? "")
+                        .font(Font.system(size: 14, weight: .medium))
+                        .foregroundColor(Color.black)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                    Text(film.nameEn ?? "")
+                        .font(Font.system(size: 12, weight: .light))
+                        .foregroundColor(Color.gray)
+                        .frame(maxWidth: .infinity, alignment: .topLeading)
+                }
+                Spacer()
+                Image(systemName: "star")
+                Text(film.rating ?? "")
+                    .font(Font.system(size: 14, weight: .light))
+                    .foregroundColor(Color.gray)
+                Image(systemName: "info.circle")
+                    .padding(20)
+                    .onTapGesture {
+                        selectedFilm = film
+                    }
+            }
+        }
+        
+    }
     
 }
